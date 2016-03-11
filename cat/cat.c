37ae99caa484af69064448e2f4d06ff5647af613
Nikolai <n.poperechnij@gmail.com>
2016-03-12 01:21:48 +0300

    cat

---
+++ cat/cat.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    
+    int cct = 1;
+    while (1) {
+        char data[2048];
+        int count = 0;
+        int size = 1024;
+        while (size > 0 && cct != 0) {
+            fflush(stdout);
+            void* tptr = data;
+            int received = read(0,tptr,size);
+            if (received == -1 ) return 1;
+            if (0 == received) cct = 0;
+            tptr += received;
+            count +=received;
+            size-=received;
+        }
+        if (count == 0) return 0;
+        int rec_count = 0;
+
+        while (count > 0) {
+            void* tptr = data;
+            int sent = write(1,tptr,count);
+            if (sent == -1) return 1;
+            if (0 == sent) return 0;
+            tptr += sent;
+            rec_count +=sent;
+            count -= sent;
+        }
+    }
+    return 0;
+    
+}
