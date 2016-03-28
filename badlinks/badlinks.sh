6864bb57a445a4cd3aa173135c7541e94514ca80
Nikolai <n.poperechnij@gmail.com>
2016-03-28 11:47:27 +0300

    fix

--- badlinks/badlinks.sh
+++ badlinks/badlinks.sh
@@ -4,7 +4,7 @@ touch -m -d '1 week ago' tempfile
 linkcheck() {
 for i in $1/*; do
     if [ ! -e "$i" -a -h "$i" -a "$tempfile" -nt "$i" ]
-    then echo "bad $i"
+    then echo "$i"
     fi
     [ -d "$i" ] && linkcheck $i
 done
