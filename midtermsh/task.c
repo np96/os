a0787e675685883dc60525a7bbc0bfa220d2c04d
Nikolai <n.poperechnij@gmail.com>
2016-04-05 17:54:02 +0300

    lol

--- midtermsh/task.c
+++ midtermsh/task.c
@@ -7,20 +7,19 @@
 #include <stdbool.h>
 #include <iostream>
 #include <fcntl.h>
-#include <signal.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <errno.h>
-#include <string>
-#include <iostream>
 #include <vector>
-#include <sstream>
+#include <sys/wait.h>
 
+std::deque<pid_t> pids;
+int current = -1;
 
 void handler(int signo) {
     if (signo == SIGINT) {
-        kill(pids)
+        if (current != -1) {
+            kill(pids[current], signo);
+        }
     }
     
 }
@@ -43,7 +42,7 @@ std::string readline() {
     while ((n = read(0, buf, size)) > 0) {
         for (int i = 0; i < n; i++) {
             if (buf[i] == '\n' || buf[i] == '\0')
-                return s+'\0';
+                return s + '\0';
             s += buf[i];
         }
         
@@ -52,55 +51,55 @@ std::string readline() {
     return s;
 }
 
-
 void proceedCommand(std::string tokens) {
     std::deque<std::string> commandList(0);
     commandList = split(tokens, ' ', commandList);
     const int len = commandList.size() + 1;
-    char * arguments[len];
-    for (int i = 0; i < commandList.size() ; ++i) {
+    char *arguments[len];
+    for (int i = 0; i < commandList.size(); ++i) {
         arguments[i] = (char *) commandList[i].c_str();
     }
-    arguments[len -1] = NULL;
+    arguments[len - 1] = NULL;
     execvp(arguments[0], arguments);
 }
 
-
-int main(int argc, char ** argv) {
+int main(int argc, char **argv) {
     signal(SIGINT, handler);
     while (1) {
         std::string args;
         args = readline();
+        if (args.length() < 1) break;
         std::deque<std::string> tokens;
         tokens = split(args, '|', tokens);
-        std::deque<int> pids(tokens.size());
-        std::deque<int*> ffd(tokens.size(), new int[2]);
+        pids.resize(tokens.size());
+        std::deque<int *> ffd(tokens.size(), new int[2]);
         for (int i = 0; i < tokens.size() - 1; ++i) {
             pipe(ffd[i]);
         }
         for (int i = 0; i < tokens.size(); ++i) {
             int id;
-            if ((id =fork()) > 0) {
+            if ((id = fork()) > 0) {
+                current = id;
                 pids[i] = id;
             } else {
                 if (i != 0) {
                     dup2(ffd[i - 1][0], STDIN_FILENO);
-                    close(ffd[i-1][1]);
+                    close(ffd[i - 1][1]);
                 }
                 if (i != tokens.size() - 1) {
                     dup2(ffd[i][1], STDOUT_FILENO);
-            }
+                }
                 close(ffd[i][0]);
                 proceedCommand(tokens[i]);
             }
             if (id < 0) return 0;
         }
-        for (int i = 0; i < tokens.size() - 1; ++i ){
+        for (int i = 0; i < tokens.size() - 1; ++i) {
             close(ffd[i][0]);
             close(ffd[i][1]);
         }
         for (int i = 0, j; i < tokens.size(); ++i) {
-            waitpid(pids[i], &j, 0);
+            waitpid(pids[i], &j, NULL);
         }
         pids.clear();
     }
